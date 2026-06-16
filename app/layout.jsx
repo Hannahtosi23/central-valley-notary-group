dc7bb479b4f3d0da0d364c887118bc138583e230
Hannahtosi23 <hannah.tosi@gmail.com>
2026-06-16 13:53:27 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -71,6 +71,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=DM+Serif+Display:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
