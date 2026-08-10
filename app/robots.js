8f41a7699771e01fe0e219c456ca0411b09107a8
Hannahtosi23 <hannah.tosi@gmail.com>
2026-08-10 12:54:37 -0700

    robots: non-www sitemap URL

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.centralvalleynotarygroup.com/sitemap.xml",
+    sitemap: "https://centralvalleynotarygroup.com/sitemap.xml",
   };
 }
